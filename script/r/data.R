ee9ef123cdabc5f6b6b36a057ffa0af3f2ce5248
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-12-06 14:52:03 +0100

    data.R: improved tables names and contents
    - introduced mnemonic naming convention
    - added with.order() everywhere
    - added icount/ucount
    - added topic

--- script/r/data.R
+++ script/r/data.R
@@ -1,82 +1,107 @@
 # Read data and create helper data
 # to be read with source()
 
+# We define a lot of tables (dataframes), some for plotting, others auxiliary.
+# Table names are of the form C_by_R where
+# C describes what is in the columns and
+# R describes what makes the identity of a row.
+#
+# Abbreviations:
+# art=article, ven=venue, sent=sentence
+# Suffixes:  N: count, S: sentences, C: characters, W:words
+
 library(sqldf)  # needs install before
 
 #----- read input data:
 datafile = "results/prestudy2-results.tsv"
-codings = read.delim(datafile)
+codings_by_sent = read.delim(datafile)
 
 #----- declare SQL fragments:
 no.duplicates = "C.codername='Lutz'"  # coder 'A' should always exist
 primary.code = "C.words is not null"
-core.codes = "('background', 'objective', 'method', 'design', 'result', 'conclusion')"
 
 #----- R constants:
-codeorder = c('h-background', 'background',
-              'h-objective', 'objective',
-              'h-design', 'design',
-              'h-method', 'method',
-              'h-result', 'result',
-              'h-conclusion', 'conclusion'
+codeorder = c('h-background', 'a-background', 'background',
+              'h-objective', 'a-objective', 'objective',
+              'h-design', 'a-design', 'design',
+              'h-method', 'a-method', 'method',
+              'h-result', 'a-result', 'result',
+              'h-conclusion', 'a-conclusion', 'conclusion'
              )
+topicorder = c('background', 'objective', 'method', 'design', 
+               'result', 'conclusion',  'other')
+
+#----- helper routines:
+
+with.order = function(df) {
+  if ("code" %in% names(df))
+    df$code = ordered(df$code, levels=rev(codeorder))
+  if ("topic" %in% names(df))
+    df$topic = ordered(df$topic, levels=rev(topicorder))
+  df
+  
+}
 
 #----- derived tables:
 '
-tbl = sqldf(sprintf("
+tbl = with.order(sqldf(sprintf("
     select x, y, z 
     from codings C
     where %s and %s
     group by fctr
-    ", fragment))
+    ", fragment)))
 '
-cat("totals:\n")
-totals = sqldf(sprintf("
-    select venue, sum(words) as words, sum(chars) as chars 
-    from codings C
+cat("totalNWC_by_ven:\n")
+totalNWC_by_ven = with.order(sqldf(sprintf("
+    select venue, 
+           sum(1) as codingsN, sum(words) as words, sum(chars) as chars,
+           sum(icount) as icount, sum(ucount) as ucount
+    from codings_by_sent C
     where %s and %s
     group by C.venue
-    ", no.duplicates, primary.code))
+    ", no.duplicates, primary.code)))
 
-cat("code totals:\n")
-code_totals = sqldf(sprintf("
-    select code, 
-           sum(words) as words, sum(chars) as chars 
-    from codings C
-    where %s and %s
-    group by code, venue
-    ", no.duplicates, primary.code))
-
-cat("abstracts:\n")
-abstracts = sqldf(sprintf("
+cat("totalNWC_by_art:\n")
+totalNWC_by_art = with.order(sqldf(sprintf("
     select citekey, venue, volume, 
-           sum(words) as words, sum(chars) as chars,
-           sum(code LIKE '%s') as num_h
-    from codings C
+           sum(1) as codingsN, sum(words) as words, sum(chars) as chars,
+           sum(icount) as icount, sum(ucount) as ucount,
+           sum(code LIKE 'h-%%') as h_N,
+           sum(code LIKE 'a-%%') as a_N,
+           sum(code LIKE 'h-%%') > 0 as is_struc,
+           sum(code LIKE '%%design') > 0 as has_design
+    from codings_by_sent C
     where %s and %s
     group by citekey
-    ", 'h-%', no.duplicates, primary.code))
-abstracts = sqldf(sprintf("
-    select *, 
-           (num_h > 0) as is_struc
-    from abstracts A
-    "))
+    ", no.duplicates, primary.code)))
 
-cat("sentences:\n")
-sentences = sqldf(sprintf("
-    select citekey, venue, volume, sidx, sum(words), sum(chars)
-    from codings C
+cat("totalNWC_by_code_ven_dsgn:\n")
+totalNWC_by_code_ven_dsgn = with.order(sqldf(sprintf("
+    select code, topic, C.venue, has_design,
+           sum(1) as codingsN, sum(C.words) as words, sum(C.chars) as chars,
+           sum(C.icount) as icount, sum(C.ucount) as ucount
+    from codings_by_sent C, totalNWC_by_art A
+    where C.citekey = A.citekey and %s and %s
+    group by C.code, C.topic, C.venue, A.has_design
+    ", no.duplicates, primary.code)))
+
+cat("totalNWC_by_sent:\n")
+totalNWC_by_sent = with.order(sqldf(sprintf("
+    select citekey, venue, volume, sidx, 
+           sum(1) as codingsN, sum(words) as words, sum(chars) as chars,
+           sum(icount) as icount, sum(ucount) as ucount
+    from codings_by_sent C
     where %s and %s
     group by citekey, sidx
-    ", no.duplicates, primary.code))
+    ", no.duplicates, primary.code)))
 
-cat("space.for.basics:\n")
-space.for.basics = sqldf(sprintf("
-    select round(100 * sum(C.words) / T.words, 1) as wordspct,
+cat("pctNWC_by_code_ven_struc:\n")
+pctNWC_by_code_ven_struc = with.order(sqldf(sprintf("
+    select round(100 * sum(1) / T.codingsN, 1) as codingspct,
+           round(100 * sum(C.words) / T.words, 1) as wordspct,
            round(100 * sum(C.chars) / T.chars, 1) as charspct,
-           C.code, C.venue, A.is_struc
-    from codings C, abstracts A, totals T
+           C.code, C.venue, is_struc, has_design
+    from codings_by_sent C, totalNWC_by_art A, totalNWC_by_ven T
     where C.citekey == A.citekey and C.venue == T.venue and %s and %s
-    group by C.code, C.venue, A.is_struc
-    ", no.duplicates, primary.code))  # TODO: include duplicates!
-space.for.basics$code = ordered(space.for.basics$code, levels=rev(codeorder))          
+    group by C.code, C.venue, A.is_struc, A.has_design
+    ", no.duplicates, primary.code)))  # TODO: include duplicates!
