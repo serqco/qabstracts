27f3f8c97045980d791a1fb0fc68256f7fb5d1c0
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2022-12-06 14:52:58 +0100

    plots.R: added plot for totalNWC_by_code_ven_dsgn

--- script/r/plots.R
+++ script/r/plots.R
@@ -1,7 +1,14 @@
 
 
-dd=space.for.basics
-where=!is.na(dd$code)
+dd=pctNWC_by_code_ven_struc
+where=(as.character(dd$code) %in% (core.codes))
 
 ggplot(dd[where,], aes(x=paste(venue), y=wordspct, fill=code)) + 
        geom_col() + theme_light()
+
+
+dd=totalNWC_by_code_ven_dsgn
+where=(dd$topic != 'none')
+
+ggplot(dd[where,], aes(x=paste(has_design), y=icount+ucount, fill=code)) + 
+  geom_col() + theme_light()
