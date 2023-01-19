44dc96b0f9a3f20e852ea34abf8394ff7c61a7ce
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-01-19 18:47:47 +0100

    download-volumes.sh with realistic commands

--- script/download-volumes.sh
+++ script/download-volumes.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Downloads the intended PDFs and metadata into volumes/*.
-# To be called from the top-level directory (with activated venv if needed).
+# To be called from the top-level directory (with an alias or activated venv etc. such that
+# the retrievelit command is available under that name).
 # Each call must include all existing corpus directories so that retrievlit can
 # avoid name collisions.
-
-retrievelit() {
-  # highly provisional calling style:
-  python /c/ws/rq/retrievelit/main.py --ieeecs $*
-}
+# It is almost certainly better to call these commands one-by-one, not as a batch.
 
 cd volumes
 
-retrievelit --mapper ComputerOrgJournal --ieeecs TSE-2021
-retrievelit --mapper Springer EMSE-2021 TSE-2021
+retrievelit --mapper Elsevier IST-2022
+retrievelit --mapper Acm --maxwait=30 TOSEM-2022 IST-2022
+retrievelit --mapper Acm --maxwait=30 --grouping=volume ICSE-2022 TOSEM-2022 IST-2022
+retrievelit --mapper ComputerOrgJournal TSE-2022 ICSE-2022 TOSEM-2022 IST-2022
+retrievelit --mapper Springer EMSE-2022 TSE-2022 ICSE-2022 TOSEM-2022 IST-2022
 
 cd ..
