e8b933016dea67ea65992673c3ca6e90bfea26e4
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-04-27 18:02:24 +0200

    qconclusions.py simplified to scan("qscript.cmd.*", ...)

--- script/qconclusions.py
+++ script/qconclusions.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
 import qscript.argparse_subcommand
-import qscript.cmd.check_codings
-import qscript.cmd.compare_codings
-import qscript.cmd.fix_encoding
-import qscript.cmd.prepare_ann 
-import qscript.cmd.select_sample
 import qconcl.extract_concl
 import qconcl.prepare_sample
 
@@ -14,13 +9,10 @@ explanation = """Handles various steps of the qconclusions study."""
 
 def main():
     parser = qscript.argparse_subcommand.ArgumentParser(epilog=explanation)
-    parser.scan(qscript.cmd.select_sample,
-                qscript.cmd.fix_encoding,
-                qscript.cmd.check_codings,
-                qscript.cmd.compare_codings,
+    parser.scan("qscript.cmd.*",
                 qconcl.extract_concl,
                 qconcl.prepare_sample,
-                strict=True)
+                strict=True, trace=False)
     parser.execute_subcommand()
 
 
