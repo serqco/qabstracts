4922d9a78a327c0e021ee36e7f1c711de626be3b
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-04-27 18:02:03 +0200

    qabstracts.py simplified to scan("qscript.cmd.*", ...)

--- script/qabstracts.py
+++ script/qabstracts.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
 import qscript.argparse_subcommand
-import qscript.cmd.check_codings
-import qscript.cmd.compare_codings
-import qscript.cmd.fix_encoding
-import qscript.cmd.prepare_ann 
-import qscript.cmd.select_sample
 import qabs.export
 import qabs.extract_abs
 import qabs.plot
@@ -16,10 +11,7 @@ explanation = """Handles various steps of the qabstracts study."""
 
 def main():
     parser = qscript.argparse_subcommand.ArgumentParser(epilog=explanation)
-    parser.scan(qscript.cmd.select_sample,
-                qscript.cmd.fix_encoding,
-                qscript.cmd.check_codings,
-                qscript.cmd.compare_codings,
+    parser.scan("qscript.cmd.*",
                 qabs.export,
                 qabs.extract_abs,
                 qabs.plot,
