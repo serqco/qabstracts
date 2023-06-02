5a5c82da594675a68120aac3004f74bc41dac2a3
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-06-02 10:24:37 +0200

    annotations_abs: empty CodebookAbstracts, AnnotationsAbstracts

---
+++ script/qabs/annotations_abs.py
@@ -0,0 +1,10 @@
+"""qabstracts-specific concrete subclasses for classes in qscript.annotations"""
+import qscript.annotations as qa
+
+class CodebookAbstracts(qa.Codebook):
+    pass
+
+
+class AnnotationsAbstracts(qa.Annotations):
+    pass
+
